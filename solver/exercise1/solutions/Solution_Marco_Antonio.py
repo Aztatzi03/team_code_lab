b5a4924470af85aec03ba46566449cb8664f2837
Aztatzi03 <marco.perezazt@alumno.buap.mx>
2026-03-17 13:54:24 -0600

    solucion parcial

---
+++ solver/exercise1/solutions/Solution_Marco_Antonio.py
@@ -0,0 +1,11 @@
+
+def cuadrado(a):
+    if type(a)=bool:
+        return print("la funcion no admite varibles logicas")
+    else:
+        return a*a
+
+    
+lista=[3, 4.678, True, 5, -4.8, "hola"]
+
+resultado=cuadrado(lista)
